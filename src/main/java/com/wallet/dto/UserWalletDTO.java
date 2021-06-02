ff7de5c6ad10d922fd856bc1fe37668a0c7a025c
Antônio Araújo <antonio.araujo@qintess.com>
2021-06-01 22:58:00 -0300

    entidade wallet

---
+++ src/main/java/com/wallet/dto/UserWalletDTO.java
@@ -0,0 +1,34 @@
+package com.wallet.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class UserWalletDTO {
+
+	private Long id;
+	
+	@NotNull(message = "Informe o ID do usuário")
+	private Long users;
+	
+	@NotNull(message = "Informe o ID da carteira")
+	private Long wallet;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUsers() {
+		return users;
+	}
+	public void setUsers(Long users) {
+		this.users = users;
+	}
+	public Long getWallet() {
+		return wallet;
+	}
+	public void setWallet(Long wallet) {
+		this.wallet = wallet;
+	}
+	
+}
---
+++ src/main/java/com/wallet/entity/UserWallet.java
@@ -0,0 +1,59 @@
+package com.wallet.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users_wallet")
+public class UserWallet implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@JoinColumn(name = "users", referencedColumnName = "id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User users;
+	
+	@JoinColumn(name = "wallet", referencedColumnName = "id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Wallet wallet;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUsers() {
+		return users;
+	}
+
+	public void setUsers(User users) {
+		this.users = users;
+	}
+
+	public Wallet getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(Wallet wallet) {
+		this.wallet = wallet;
+	}
+	
+}
